f247c312d18b15e79b385802dbdea7b1c3e0d1d1
TimW29 <timwijnandswerk@gmail.com>
2024-05-12 16:34:49 +0200

    composer update
    Heb nu /vendor/composer toegevoegd. Hopelijk stopt het nu met deze aanpassingen pushen

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '89bad8d0df060f5850b31efade60bb35b7253d10',
+        'reference' => 'fcc1772a86051af11a288beb670a1944ac653412',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -430,7 +430,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '89bad8d0df060f5850b31efade60bb35b7253d10',
+            'reference' => 'fcc1772a86051af11a288beb670a1944ac653412',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
